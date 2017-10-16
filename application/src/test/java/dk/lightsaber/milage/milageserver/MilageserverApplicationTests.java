ca005234275abaf13a467d1456e83bcfb3d6fbaf
RolfKristensen <rolf@lightsaber.dk>
2017-10-16 09:08:44 +0200

    spring boot init

---
+++ application/src/test/java/dk/lightsaber/milage/milageserver/MilageserverApplicationTests.java
@@ -0,0 +1,16 @@
+package dk.lightsaber.milage.milageserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MilageserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
