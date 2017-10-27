909263c5f8837b707241231734548d646111d640
RolfKristensen <rolf@lightsaber.dk>
2017-10-27 15:49:38 +0200

    fix unit test error

--- src/main/java/dk/lightsaber/milage/server/config/Default.java
+++ src/main/java/dk/lightsaber/milage/server/config/Default.java
@@ -2,9 +2,23 @@ package dk.lightsaber.milage.server.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
+@EnableJpaRepositories(
+        basePackages = {"dk.lightsaber.milage.server.pdo.repository"}
+)
+@EnableTransactionManagement
 public class Default {
     private final static Logger LOGGER = LoggerFactory.getLogger(Default.class.getName());
 
@@ -12,5 +26,39 @@ public class Default {
 
     public Default(ApplicationProperties props) {
         this.props = props;
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory(DataSource dataSource) {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("dk.lightsaber.milage.server.pdo.model");
+        factory.setDataSource(dataSource);
+        factory.afterPropertiesSet();
+
+        return factory.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+
+    @Bean
+    public DbConfig dbConfig() {
+        if (props.getDbPropertyFilePath() != null) {
+            LOGGER.info("Using DB properties file: " + props.getDbPropertyFilePath());
+            return new DbPropertiesFileConfig(props.getDbPropertyFilePath());
+        } else {
+            LOGGER.info("No DB properties file defined");
+            // TODO: add support for ENV config
+            return null;
+        }
     }
 }
--- src/main/java/dk/lightsaber/milage/server/config/Psqldb.java
+++ src/main/java/dk/lightsaber/milage/server/config/Psqldb.java
@@ -6,30 +6,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
 @Profile("psqldb")
-@EnableJpaRepositories(
-        basePackages = {"dk.lightsaber.milage.server.pdo.repository"}
-)
-@EnableTransactionManagement
 public class Psqldb {
     private final static Logger LOGGER = LoggerFactory.getLogger(Default.class.getName());
     private final DbConfig config;
-    private final ApplicationProperties properties;
 
-    public Psqldb(ApplicationProperties properties) {
-        this.properties = properties;
-        this.config = dbConfig();
+    public Psqldb(DbConfig dbConfig) {
+        this.config = dbConfig;
     }
 
     @Bean
@@ -43,36 +30,4 @@ public class Psqldb {
         return ds;
     }
 
-    @Bean
-    public EntityManagerFactory entityManagerFactory(DataSource dataSource) {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan("dk.lightsaber.milage.server.pdo.model");
-        factory.setDataSource(dataSource);
-        factory.afterPropertiesSet();
-
-        return factory.getObject();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-
-        JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory);
-        return txManager;
-    }
-
-    private DbConfig dbConfig() {
-        if (properties.getDbPropertyFilePath() != null) {
-            LOGGER.info("Using DB properties file: " + properties.getDbPropertyFilePath());
-            return new DbPropertiesFileConfig(properties.getDbPropertyFilePath());
-        } else {
-            LOGGER.info("No DB properties file defined");
-            // TODO: add support for ENV config
-            return null;
-        }
-    }
 }
